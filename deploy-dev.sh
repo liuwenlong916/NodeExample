a95bda7aa211cdc787facd5bedf7ee2d0591b9de
1141657011@qq.com <1141657011@qq.com>
2024-05-29 18:00:09 +0800

    fix

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,18 +1,18 @@
 echo Deploy Project
 
 # 获取最新版代码
-git pull
-echo 代码更新成功
+git pull && sudo docker-compose down && sudo docker rmi nodeexample_app-pm2 && sudo docker-compose up -d --force-recreate --build
+# echo 代码更新成功
 # docker-compose up -d --force-recreate --build
 # 强制重新编译容器
-sudo docker-compose down
-echo docker服务停止
+# sudo docker-compose down
+# echo docker服务停止
 
-sudo docker rmi nodeexample_app-pm2 
-echo 原始镜像删除
+# sudo docker rmi nodeexample_app-pm2 
+# echo 原始镜像删除
 
-sudo docker-compose up -d --force-recreate --build
-echo docker服务部署成功
+# sudo docker-compose up -d --force-recreate --build
+# echo docker服务部署成功
 # 定制镜像 Dockerfile
 # docker build -t myapp:pm2 ./backend
 
