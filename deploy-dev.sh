3eadcb7743cb6723668ed950bbcd5845daefd9e8
1141657011@qq.com <1141657011@qq.com>
2024-05-30 10:34:56 +0800

    fix

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,24 +1,25 @@
-echo Deploy Project
+echo "Deploy Project"
 
-# TODO 如何保证代码更新完之后 重新打包
 # 获取最新版代码
-git pull 
-# && sudo docker-compose down && sudo docker rmi nodeexample_app-pm2 && sudo docker-compose up -d --force-recreate --build
-# echo 代码更新成功
+git pull
+if [ $? -ne 0 ]; then
+    echo "git pull failed" >> deploy.log
+    echo "git pull failed. Check deploy.log for details."
+    exit 1
+fi
+echo "代码更新成功" >> deploy.log
 
 # 强制重新编译容器
 sudo docker-compose down
-# echo docker服务停止
 
-# sudo docker rmi nodeexample_app-pm2 
-# echo 原始镜像删除
+# sudo docker rmi nodeexample_app-pm2
+# echo "原始镜像删除" >> deploy.log
 
 sudo docker-compose up -d --force-recreate --build
-# echo docker服务部署成功
-# 定制镜像 Dockerfile
-# docker build -t myapp:pm2 ./backend
-
-# 重启启动容器
-# docker stop myoffice
-# docker rm myoffice
-# docker run --name myoffice -p 3000:3000  -d myoffice:pm2
+if [ $? -eq 0 ]; then
+    echo "docker服务部署成功" >> deploy.log
+else
+    echo "docker-compose up failed" >> deploy.log
+    echo "docker-compose up failed. Check deploy.log for details."
+    exit 1
+fi
