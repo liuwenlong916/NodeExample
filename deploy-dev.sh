86fce0b802fefba13bdf9f9a692329ec49ca761f
1141657011@qq.com <1141657011@qq.com>
2024-06-05 17:45:38 +0800

    FIX

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -14,15 +14,14 @@ echo "代码更新成功" >> deploy.log
 # 强制重新编译容器
 sudo docker-compose down
 
-# 删除无用镜像
- echo y | sudo docker image prune
-
 # sudo docker rmi nodeexample_app-pm2
 # echo "原始镜像删除" >> deploy.log
 
 sudo docker-compose up -d --force-recreate --build
 if [ $? -eq 0 ]; then
     echo "docker服务部署成功" >> deploy.log
+    # 删除无用镜像
+    echo y | sudo docker image prune
 else
     echo "docker-compose up failed" >> deploy.log
     echo "docker-compose up failed. Check deploy.log for details."
