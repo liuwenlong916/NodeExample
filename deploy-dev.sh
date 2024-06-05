67b19d6b2884e334f8ab9363f7f23d5b1ddfd432
1141657011@qq.com <1141657011@qq.com>
2024-06-05 17:58:32 +0800

    fix

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -7,21 +7,26 @@ git pull
 if [ $? -ne 0 ]; then
     echo "git pull failed" >> deploy.log
     echo "git pull failed. Check deploy.log for details."
-    exit 1
+    git pull
+    if [ $? -ne 0 ]; then
+        echo "git pull failed" >> deploy.log
+        echo "git pull failed. Check deploy.log for details."
+        exit 1
+    fi
 fi
 echo "代码更新成功" >> deploy.log
 
 # 强制重新编译容器
 sudo docker-compose down
 
-# sudo docker rmi nodeexample_app-pm2
-# echo "原始镜像删除" >> deploy.log
+# 删除无用镜像
+echo y | sudo docker image prune
+sudo docker rmi nodeexample_app-pm2
+echo "原始镜像删除" >> deploy.log
 
 sudo docker-compose up -d --force-recreate --build
 if [ $? -eq 0 ]; then
     echo "docker服务部署成功" >> deploy.log
-    # 删除无用镜像
-    echo y | sudo docker image prune
 else
     echo "docker-compose up failed" >> deploy.log
     echo "docker-compose up failed. Check deploy.log for details."
