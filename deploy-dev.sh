e6200c21e99410dfdede1ebfb438f1bb95fb7053
1141657011@qq.com <1141657011@qq.com>
2024-06-06 16:33:08 +0800

    fix

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,20 +1,16 @@
-
-date +%Y/%m/%d_%H:%M:%S >> deploy.log
-echo "Deploy Project" >> deploy.log
+echo "Deploy Project : $(date +%Y/%m/%d_%H:%M:%S)" 
 
 # 获取最新版代码
 git pull
 if [ $? -ne 0 ]; then
-    echo "git pull failed" >> deploy.log
-    echo "git pull failed. Check deploy.log for details."
+    echo "git pull failed"
     git pull
     if [ $? -ne 0 ]; then
-        echo "git pull failed" >> deploy.log
-        echo "git pull failed. Check deploy.log for details."
+        echo "git pull failed" 
         exit 1
     fi
 fi
-echo "代码更新成功" >> deploy.log
+echo "代码更新成功" 
 
 # 强制重新编译容器
 sudo docker-compose down
@@ -22,16 +18,16 @@ sudo docker-compose down
 # 删除无用镜像
 echo y | sudo docker image prune
 # sudo docker rmi nodeexample_app-pm2
-# echo "原始镜像删除" >> deploy.log
+# echo "原始镜像删除"
 
 sudo docker-compose up -d --force-recreate --build
 if [ $? -eq 0 ]; then
-    echo "docker服务部署成功" >> deploy.log
+    echo "docker服务部署成功" 
 else
-    echo "docker-compose up failed" >> deploy.log
-    echo "docker-compose up failed. Check deploy.log for details."
+    echo "docker-compose up failed. "
+    echo "异常信息: $?"
     exit 1
 fi
-echo "结束时间:" >> deploy.log
-date +%Y/%m/%d_%H:%M:%S >> deploy.log
-echo "======================" >> deploy.log
+echo "Deployed project : $(date +%Y/%m/%d_%H:%M:%S)" 
+
+echo "======================" 
